4b113c35df7211eb966e11d498f377000e638b6f
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-06-04 08:29:58 +0000

    Deploying to gh-pages from @ ts4ed/team-work-01@8556d2befdf8e54ca1ba8811860204c95621056c 🚀

---
+++ index.7a2ebdb7.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{slidesPerGroup:1,slidesPerView:3,autoHeight:!0,initialSlide:1,centeredSlides:!0,slideToClickedSlide:!0,loop:!0,breakpoints:{150:{slidesPerView:1},768:{slidesPerView:3}},pagination:{el:".swiper-pagination",type:"fraction"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.7a2ebdb7.js.map
